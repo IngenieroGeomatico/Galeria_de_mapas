665b2996156cd12111b08d2225ea3b018365ef16
ingenieroGeomatico <aurearagon@gmail.com>
2024-10-14 20:30:25 +0200

    new file:   js/lib/transform/csv.js 	modified:   mapas/CalidadAireMadridTiempoReal.html

---
+++ js/lib/transform/csv.js
@@ -0,0 +1,62 @@
+
+function csvToJson(csvString) {
+    const rows = csvString
+        .split("\n");
+
+    const headers = rows[0]
+        .split(";");
+
+    const jsonData = [];
+    for (let i = 1; i < rows.length; i++) {
+
+        const values = rows[i]
+            .split(";");
+
+        const obj = {};
+
+        for (let j = 0; j < headers.length; j++) {
+
+            const key = headers[j]
+                .trim();
+            const value = values[j]
+                .trim();
+
+            obj[key] = value;
+        }
+
+        jsonData.push(obj);
+    }
+    return JSON.stringify(jsonData);
+}
+
+
+function csvToGeoJson(csvString) {
+    const rows = csvString
+        .split("\n");
+
+    const headers = rows[0]
+        .split(";");
+
+    const jsonData = [];
+    for (let i = 1; i < rows.length; i++) {
+
+        const values = rows[i]
+            .split(";");
+
+        const obj = {};
+
+        for (let j = 0; j < headers.length; j++) {
+
+            const key = headers[j]
+                .trim();
+            const value = values[j]
+                .trim();
+
+            obj[key] = value;
+        }
+
+        jsonData.push(obj);
+    }
+    return JSON.stringify(jsonData);
+}
+
