e90bf6fcefcb59b190afcf469e5963df9efac3f9
ingenieroGeomatico <aurearagon@gmail.com>
2025-01-23 20:07:56 +0100

    Se añade estilo a las 3 capas

--- mapas/PuntosHistoricosMadrid/js/mapa.js
+++ mapas/PuntosHistoricosMadrid/js/mapa.js
@@ -50,6 +50,7 @@ mapajs.addQuickLayers('Base_IGNBaseTodo_TMS_2')
 
 
 // Estilos
+
 let clusterOptionsMonumentos = {
   ranges: [{
     min: 2,
@@ -135,6 +136,250 @@ let vendorParameters = {
 //generamos un cluster personalizado
 let styleCluster_Monumentos = new M.style.Cluster(clusterOptionsMonumentos, vendorParameters);
 
+let estilo_base_Monumentos = new M.style.Generic({
+  point: {
+    icon: {
+      // Forma del fontsymbol.
+      // BAN(cículo)|BLAZON(diálogo cuadrado)|BUBBLE(diálogo redondo)|CIRCLE(círculo)|LOZENGE(diamante)|MARKER(diálogo redondeado)
+      // NONE(ninguno)|SHIELD(escudo)|SIGN(triángulo)|SQUARE(cuadrado)|TRIANGLE(triángulo invertido)
+      form: M.style.form.SHIELD,
+      // Tamaño de la fuente
+      fontsize: 0.7,
+      // Clase fuente
+      class: 'M',
+      // Tamaño del radio
+      radius: 15,
+      // Giro del icono en radianes
+      rotation: 0,
+      // Activar rotación con dispositivo
+      rotate: false,
+      // Desplazamiento en píxeles en los ejes x,y
+      color: 'red', // Hexadecimal, nominal
+      // Desplazamiento
+      offset: [0, 0],
+      // Color de relleno. Hexadecimal, nominal
+      fill: 'orange',
+      // Transparencia. 0(transparente)|1(opaco) 
+      opacity: 0.9,
+    },
+  }
+});
+
+let compositeMonumentos = styleCluster_Monumentos.add(estilo_base_Monumentos);
+
+
+
+let clusterOptionsPlacas = {
+  ranges: [{
+    min: 2,
+    max: 5,
+    style: new M.style.Generic({
+      point: {
+        stroke: {
+          color: '#5789aa'
+        },
+        fill: {
+          color: '#79daf7',
+        },
+        radius: 10
+      }
+    })
+  }, {
+    min: 5,
+    max: 9,
+    style: new M.style.Generic({
+      point: {
+        stroke: {
+          color: '#5789aa'
+        },
+        fill: {
+          color: '#46a5c2',
+        },
+        radius: 20
+      }
+    })
+  }, {
+    min: 10,
+    max: 49,
+    style: new M.style.Generic({
+      point: {
+        stroke: {
+          color: '#5789aa'
+        },
+        fill: {
+          color: '#157a99',
+        },
+        radius: 30
+      }
+    })
+  }, {
+    min: 50,
+    max: 9999,
+    style: new M.style.Generic({
+      point: {
+        stroke: {
+          color: '#5789aa'
+        },
+        fill: {
+          color: '#034357',
+        },
+        radius: 40
+      }
+    })
+  }
+  ],
+  animated: true,
+  hoverInteraction: true,
+  displayAmount: true,
+  selectInteraction: true,
+  distance: 80,
+  label: {
+    font: 'bold 19px Comic Sans MS',
+    color: '#FFFFFF'
+  }
+};
+
+//generamos un cluster personalizado
+let styleCluster_Placas= new M.style.Cluster(clusterOptionsPlacas, vendorParameters);
+
+let estilo_base_Placas = new M.style.Generic({
+  point: {
+    icon: {
+      // Forma del fontsymbol.
+      // BAN(cículo)|BLAZON(diálogo cuadrado)|BUBBLE(diálogo redondo)|CIRCLE(círculo)|LOZENGE(diamante)|MARKER(diálogo redondeado)
+      // NONE(ninguno)|SHIELD(escudo)|SIGN(triángulo)|SQUARE(cuadrado)|TRIANGLE(triángulo invertido)
+      form: M.style.form.LOZENGE,
+      // Tamaño de la fuente
+      fontsize: 0.7,
+      // Clase fuente
+      class: 'P',
+      // Tamaño del radio
+      radius: 15,
+      // Giro del icono en radianes
+      rotation: 0,
+      // Activar rotación con dispositivo
+      rotate: false,
+      // Desplazamiento en píxeles en los ejes x,y
+      color: 'black', // Hexadecimal, nominal
+      // Desplazamiento
+      offset: [0, 0],
+      // Color de relleno. Hexadecimal, nominal
+      fill: '#FFD600',
+      // Transparencia. 0(transparente)|1(opaco) 
+      opacity: 0.9,
+    },
+  }
+});
+
+let compositePlacas = styleCluster_Placas.add(estilo_base_Placas);
+
+
+
+let clusterOptionsStonh = {
+  ranges: [{
+    min: 2,
+    max: 5,
+    style: new M.style.Generic({
+      point: {
+        stroke: {
+          color: '#5789aa'
+        },
+        fill: {
+          color: '#79daf7',
+        },
+        radius: 10
+      }
+    })
+  }, {
+    min: 5,
+    max: 9,
+    style: new M.style.Generic({
+      point: {
+        stroke: {
+          color: '#5789aa'
+        },
+        fill: {
+          color: '#46a5c2',
+        },
+        radius: 20
+      }
+    })
+  }, {
+    min: 10,
+    max: 49,
+    style: new M.style.Generic({
+      point: {
+        stroke: {
+          color: '#5789aa'
+        },
+        fill: {
+          color: '#157a99',
+        },
+        radius: 30
+      }
+    })
+  }, {
+    min: 50,
+    max: 9999,
+    style: new M.style.Generic({
+      point: {
+        stroke: {
+          color: '#5789aa'
+        },
+        fill: {
+          color: '#034357',
+        },
+        radius: 40
+      }
+    })
+  }
+  ],
+  animated: true,
+  hoverInteraction: true,
+  displayAmount: true,
+  selectInteraction: true,
+  distance: 80,
+  label: {
+    font: 'bold 19px Comic Sans MS',
+    color: '#FFFFFF'
+  }
+};
+
+//generamos un cluster personalizado
+let styleCluster_Stonh = new M.style.Cluster(clusterOptionsStonh, vendorParameters);
+
+let estilo_base_Stonh = new M.style.Generic({
+  point: {
+    icon: {
+      // Forma del fontsymbol.
+      // BAN(cículo)|BLAZON(diálogo cuadrado)|BUBBLE(diálogo redondo)|CIRCLE(círculo)|LOZENGE(diamante)|MARKER(diálogo redondeado)
+      // NONE(ninguno)|SHIELD(escudo)|SIGN(triángulo)|SQUARE(cuadrado)|TRIANGLE(triángulo invertido)
+      form: M.style.form.SQUARE,
+      // Tamaño de la fuente
+      fontsize: 0.7,
+      // Clase fuente
+      class: 'S',
+      // Tamaño del radio
+      radius: 15,
+      // Giro del icono en radianes
+      rotation: 0,
+      // Activar rotación con dispositivo
+      rotate: false,
+      // Desplazamiento en píxeles en los ejes x,y
+      color: 'black', // Hexadecimal, nominal
+      // Desplazamiento
+      offset: [0, 0],
+      // Color de relleno. Hexadecimal, nominal
+      fill: '#c9be1c',
+      // Transparencia. 0(transparente)|1(opaco) 
+      opacity: 0.9,
+    },
+  }
+});
+
+let compositeStonh= styleCluster_Stonh.add(estilo_base_Stonh);
+
+
 geojsonData = myFunction_GetData()
 geojsonData.then(() => {
 
@@ -150,6 +395,7 @@ geojsonData.then(() => {
       description: " <a style='color: #0000FF' href='https://datos.madrid.es/portal/site/egob' target='_blank'>Ayuntamiento de Madrid</a> "
     }
   }, {
+    visibility:true,
     // style:estiloEstacion
   })
   capaMonumentos.setStyle(styleCluster_Monumentos)
@@ -173,6 +419,7 @@ geojsonData.then(() => {
       description: " <a style='color: #0000FF' href='https://datos.madrid.es/portal/site/egob' target='_blank'>Ayuntamiento de Madrid</a> "
     }
   }, {
+    visibility:false,
     // style:estiloEstacion
   })
   // capaPlacas.setStyle(styleCluster_Monumentos)
@@ -183,6 +430,14 @@ geojsonData.then(() => {
   //     mapajs.getPopup().hide()
   //   }
   // });
+  capaPlacas.setStyle(styleCluster_Placas)
+  capaPlacas.on(M.evt.SELECT_FEATURES, function (features, evt) {
+    // se puede comprobar si el elemento seleccionado es un cluster o no
+    if (features[0] instanceof M.ClusteredFeature) {
+      console.log('Es un cluster');
+      mapajs.getPopup().hide()
+    }
+  });
   mapajs.addLayers(capaPlacas)
 
 
@@ -196,6 +451,7 @@ geojsonData.then(() => {
       description: " <a style='color: #0000FF' href='https://datos.madrid.es/portal/site/egob' target='_blank'>Ayuntamiento de Madrid</a> "
     }
   }, {
+    visibility:false,
     // style:estiloEstacion
   })
   // capaPlacas.setStyle(styleCluster_Monumentos)
@@ -206,6 +462,14 @@ geojsonData.then(() => {
   //     mapajs.getPopup().hide()
   //   }
   // });
+  capaStolpersteine.setStyle(styleCluster_Stonh)
+  capaStolpersteine.on(M.evt.SELECT_FEATURES, function (features, evt) {
+    // se puede comprobar si el elemento seleccionado es un cluster o no
+    if (features[0] instanceof M.ClusteredFeature) {
+      console.log('Es un cluster');
+      mapajs.getPopup().hide()
+    }
+  });
   mapajs.addLayers(capaStolpersteine)
 
 })
