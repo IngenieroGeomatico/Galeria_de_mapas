88d20a27d65385635a03452cbf82a64f3c365b2e
ingenieroGeomatico <aurearagon@gmail.com>
2025-02-18 18:43:44 +0100

    Se añade arrastrador bonito y se almacenan los archivos en un objeto

--- mapas/GDAL_JS/js/drag&Drop.js
+++ mapas/GDAL_JS/js/drag&Drop.js
@@ -1,15 +1,47 @@
+var fileUpload = document.querySelector(".upload");
 
-  function readUrl(input) {
+var filesObj={}
+
+var n = 1
+
+function readUrl(input) {
   
-    if (input.files && input.files[0]) {
-      let reader = new FileReader();
-      reader.onload = (e) => {
-        let imgData = e.target.result;
-        let imgName = input.files[0].name;
-        input.setAttribute("data-title", imgName);
-        console.log(e.target.result);
-      }
-      reader.readAsDataURL(input.files[0]);
+  if (input.files && input.files[0]) {
+    let reader = new FileReader();
+    reader.onload = (e) => {
+      let imgData = e.target.result;
+      let imgName = input.files[0].name;
+      input.setAttribute("data-title", imgName);
+      console.log(e.target.result);
+      setTimeout(() => fileUpload.classList.add("done"), 3000);
+      setTimeout(() => fileUpload.classList.remove("done"), 6000);
+      setTimeout(() => fileUpload.classList.remove("drop"), 6000);
+      reader.name = imgName
+      console.log(reader)
+      filesObj[n] = reader
+      n +=1
     }
-  
+    reader.readAsDataURL(input.files[0]);
   }
+
+}
+
+
+fileUpload.addEventListener("dragover", function() {
+  this.classList.add("drag");
+  this.classList.remove("drop", "done");
+});
+
+fileUpload.addEventListener("dragleave", function() {
+  this.classList.remove("drag");
+});
+
+fileUpload.addEventListener("drop", start, false);
+fileUpload.addEventListener("change", start, false);
+
+function start() { 
+  this.classList.remove("drag");
+  this.classList.add("drop");
+  
+}
+
