b0d9f09d66ff176815ed6b90a5729b6e834ef4c2
ingenieroGeomatico <aurearagon@gmail.com>
2025-02-19 18:58:13 +0100

    Se comienza con la parte de incoporar gdal

--- mapas/GDAL_JS/js/drag&Drop.js
+++ mapas/GDAL_JS/js/drag&Drop.js
@@ -1,27 +1,47 @@
 var fileUpload = document.querySelector(".upload");
-
 var filesObj={}
-
 var n = 1
+let gdal;  // Variable global
 
 function readUrl(input) {
+
+  console.log('u - - - - -',gdal);  // Aquí ya puedes acceder a la variable gdal
   
-  if (input.files && input.files[0]) {
-    let reader = new FileReader();
-    reader.onload = (e) => {
-      let imgData = e.target.result;
-      let imgName = input.files[0].name;
-      input.setAttribute("data-title", imgName);
-      console.log(e.target.result);
-      setTimeout(() => fileUpload.classList.add("done"), 3000);
-      setTimeout(() => fileUpload.classList.remove("done"), 6000);
-      setTimeout(() => fileUpload.classList.remove("drop"), 6000);
-      reader.name = imgName
-      // console.log(reader)
-      filesObj[n] = reader
-      n+=1
-    }
-    reader.readAsDataURL(input.files[0]);
+  const file = input.files[0];
+  if (file && gdal) {
+      const reader = new FileReader();
+      reader.readAsArrayBuffer(file);
+      
+      reader.onload = () => {
+          const arrayBuffer = reader.result;
+          console.log("Archivo cargado como ArrayBuffer:", arrayBuffer);
+
+          if(input.value.includes('zip' )){
+            gdal.Module.FS.writeFile('/input/file.zip', new Int8Array(arrayBuffer) );
+            dataset = gdal.open('/input/file.zip',[],['vsizip'])
+
+          }
+          else{
+              gdal.Module.FS.writeFile('/input/file.file', new Int8Array(arrayBuffer) );
+              dataset = gdal.open('/input/file.file')
+          }
+
+
+
+          let imgName = file.name
+          arrayBuffer.name = imgName
+          filesObj[n] = arrayBuffer
+          n+=1
+
+          setTimeout(() => fileUpload.classList.add("done"), 1000);
+          setTimeout(() => fileUpload.classList.remove("done"), 3000);
+          setTimeout(() => fileUpload.classList.remove("drop"), 3000);
+
+      };
+
+      reader.onerror = () => {
+          console.error("Error al leer el archivo");
+      };
   }
 
 }
@@ -45,3 +65,24 @@ function start() {
   
 }
 
+
+async function initGdalJS() {
+  // Esperamos a que gdal se haya inicializado
+  await initGdalJs({ path: 'https://cdn.jsdelivr.net/npm/gdal3.js@2.8.1/dist/package', useWorker: false })
+    .then((Gdal) => {
+      gdal = Gdal;
+      const count = Object.keys(Gdal.drivers.raster).length + Object.keys(Gdal.drivers.vector).length;
+      console.log(count);
+      console.log(Gdal.drivers);
+    })
+    .catch((err) => {
+      console.error("Error al inicializar GDAL:", err);
+    });
+
+  const SVGCarga = document.getElementById("cargaSVG")
+  SVGCarga.hidden = true
+  SVGCarga.style.visibility = "hidden"
+  console.log('o- - - - -', gdal);  // Aquí ya puedes acceder a la variable gdal
+}
+
+initGdalJS()
