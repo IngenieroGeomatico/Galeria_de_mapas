dc34d6ddd62cd9bebb4749160bccb2d824561ea7
ingenieroGeomatico <aurearagon@gmail.com>
2026-09-02 11:03:08 +0200

    Parametrizar miniaturas de capas base en cada visualizador
    Copia la carpeta img/ de MapasBase (IGNBase.png, imagen.png, hibrido.png) a los visualizadores que usan la ext de capas base: GJSONdesdeURL, plantilla, constelaciones y satelites. Anade imgPreview a cada capa base de su backgroundlayers apuntando a la imagen local.

--- mapas/plantilla/js/mapa.js
+++ mapas/plantilla/js/mapa.js
@@ -199,6 +199,7 @@ function updateConfigBaseLayer() {
     {
       "id": "mapa",
       "title": "Callejero",
+      "imgPreview": "img/IGNBase.png",
       "layers": [
         "QUICK*Base_IGNBaseTodo_TMS_2"
       ]
@@ -206,6 +207,7 @@ function updateConfigBaseLayer() {
     {
       "id": "imagen",
       "title": "Imagen",
+      "imgPreview": "img/imagen.png",
       "layers": [
         "QUICK*BASE_PNOA_MA_TMS"
       ]
