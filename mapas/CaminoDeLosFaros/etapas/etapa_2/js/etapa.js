2feab75b9e6e563611a0d3b844718463f832d467
ingenieroGeomatico <aurearagon@gmail.com>
2026-09-08 16:46:11 +0200

    Migracion a apiidee: las 8 etapas del Camino de los Faros - sustituye la API-CNIG apiign por apiidee e integra las extensiones cambioImpl/selectorCapas/layergroup

--- mapas/CaminoDeLosFaros/etapas/etapa_2/js/etapa.js
+++ mapas/CaminoDeLosFaros/etapas/etapa_2/js/etapa.js
@@ -1,6 +1,57 @@
 
+function updateConfigBaseLayer() {
+  Base_IGNBaseTodo_TMS_2 = new IDEE.layer.TMS({
+    url: 'https://tms-ign-base.idee.es/1.0.0/IGNBaseTodo/{z}/{x}/{-y}.jpeg',
+    legend: 'IGNBaseTodo_2',
+    visible: true,
+    isBase: true,
+    tileGridMaxZoom: 17,
+    name: 'IGNBaseTodo_2',
+    attribution: '<p><b>Mapa base</b>: <a style="color: #0000FF" href="https://www.scne.es" target="_blank">SCNE</a></p>',
+  }, {
+    crossOrigin: 'anonymous',
+    displayInLayerSwitcher: false,
+  })
+
+  IDEE.addQuickLayers({
+    Base_IGNBaseTodo_TMS_2: Base_IGNBaseTodo_TMS_2
+  })
+
+  tms_2 = {
+    "base": "QUICK*Base_IGNBaseTodo_TMS_2"
+  }
+
+  IDEE.config("tms", tms_2)
+  IDEE.config.backgroundlayers = [
+    {
+      "id": "mapa",
+      "title": "Callejero",
+      "imgPreview": "../../../../mapas/MapaBase/img/IGNBase.png",
+      "layers": [
+        "QUICK*Base_IGNBaseTodo_TMS_2"
+      ]
+    },
+    {
+      "id": "imagen",
+      "title": "Imagen",
+      "imgPreview": "../../../../mapas/MapaBase/img/imagen.png",
+      "layers": [
+        "QUICK*BASE_PNOA_MA_TMS"
+      ]
+    }
+  ]
+
+  IDEE.proxy(false);
+
+  return
+}
+
+function mapa() {
+
+updateConfigBaseLayer()
+
 // Configuración del mapa
-const mapjs = IDEE.map({
+mapjs = IDEE.map({
     container: 'mapjs', //id del contenedor del mapa
     controls: ['scale*true', 'rotate', 'location', 'backgroundlayers'],
     zoom: 8,
@@ -108,8 +159,18 @@ ruta.on(IDEE.evt.LOAD, (features) => {
 
 
 // Configuración de los plugins
-const mp8 = new IDEE.plugin.Layerswitcher({
-    collapsed: true,
-    collapsible: true,
-});
-mapjs.addPlugin(mp8);
+  mapjs.addPlugin(new miPlugin_cambioImpl({
+    buttonTitle: 'cambiar impl :)',
+    mapsFunction: mapa,
+    sameMap: true,
+    shareView: true,
+    shareLayers: true
+  }));
+  mapjs.addPlugin(new miPlugin_baseLayer({ rows: 1 }));
+  mapjs.addPlugin(new miPlugin_layerSwitcher());
+
+  return mapjs
+
+}
+
+mapa()
